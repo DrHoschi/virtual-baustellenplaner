80b421ecb30ff510cd2d00287a5ab88a4fc7284c
DrHoschi <mann_alexander2000@yahoo.de>
2026-02-08 21:05:49 +0100

    Update loader.js

--- core/loader.js
+++ core/loader.js
@@ -31,7 +31,7 @@ import { createStore } from "../app/store.js";
 import { createRegistry } from "../app/registry.js";
 
 import { createFeatureGate } from "./featureGate.js";
-import { loadManifestPack } from "manifest-pack.js";
+import { loadManifestPack } from "./manifest-pack.js";
 
 import { renderMenu } from "../app/ui/menu.js";
 import { createPanelRegistry } from "../ui/panels/panel-registry.js";
