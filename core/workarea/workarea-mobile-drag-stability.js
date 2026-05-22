3f4ca79b82ac5adf30a0f66e2ff5851260a3beae
DrHoschi <mann_alexander2000@yahoo.de>
2026-05-22 21:06:03 +0200

    Delete core/workarea/workarea-mobile-drag-stability.js

--- core/workarea/workarea-mobile-drag-stability.js
+++
@@ -1,7 +0,0 @@
-/*
- * Unified Workarea Mobile Drag Stability
- *
- * Reexportiert die aktuellste stabile Version `workarea-mobile-drag-stability.v1.js`.
- */
-
-export * from "../workarea-mobile-drag-stability.v1.js";
