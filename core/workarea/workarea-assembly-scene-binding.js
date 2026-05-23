9b0f0ebf6b14086e14d3fefde7afb99471278b6c
DrHoschi <mann_alexander2000@yahoo.de>
2026-05-23 18:56:23 +0200

    Delete core/workarea/workarea-assembly-scene-binding.js

--- core/workarea/workarea-assembly-scene-binding.js
+++
@@ -1,7 +0,0 @@
-/*
- * Unified Workarea Assembly‑Scene Binding
- *
- * Reexportiert die aktuellste stabile Version `workarea-assembly-scene-binding.v1.js`.
- */
-
-export * from "../workarea-assembly-scene-binding.v1.js";
