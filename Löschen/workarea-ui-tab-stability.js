3322308ffaf48fd49b23aa0f85afdecac873d8e5
DrHoschi <mann_alexander2000@yahoo.de>
2026-05-24 14:24:14 +0200

    Delete Löschen directory

--- Löschen/workarea-ui-tab-stability.js
+++
@@ -1,7 +0,0 @@
-/*
- * Unified Workarea UI Tab Stability
- *
- * Reexportiert die aktuellste stabile Version `workarea-ui-tab-stability.v8.js`.
- */
-
-export * from "../workarea-ui-tab-stability.v8.js";
